f21d46adf8bad46688b58959cb0bbf0656d06861
5tifl3r <hteinaungwin17@gmail.com>
2020-10-10 16:50:27 +0630

    servlet part1

---
+++ src/RegServlet.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegServlet
+ */
+@WebServlet("/RegServlet")
+public class RegServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		String dob=request.getParameter("dob");
+		String gender=request.getParameter("gender");
+		String [] subject=request.getParameterValues("subject");
+
+		out.println("Name :"+" "+name);
+		out.println("DOB :"+" "+dob);
+		out.println("Gender :"+" "+gender);
+
+		for(String s:subject)
+			out.println(s);
+	}
+
+}
